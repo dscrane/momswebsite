7ecfedf4f7a0c035dc122592ec7bd44011e8b27e
dscrane <daegancrane@gmail.com>
2020-04-26 09:38:21 -0400

    you guessed it more form changes

--- scripts/form-data.php
+++ scripts/form-data.php
@@ -1,4 +1,12 @@
 if ( isset( $_POST['submit'] ) ) {
 
-  echo $_REQUEST
+  $firstname = $_REQUEST['name'][0];
+  $lastname = $_REQUEST['name'][1];
+  $email = $_REQUEST['email'];
+  $phone = $_REQUEST['phone'];
+  $message = $_REQUEST['message'];
+  
+
+  echo '{$firstname . '/n' . $lastname . '/n' . $email . '/n' . $phone . '/n' . $message}'
+  
 }
