99d725703f3995240843b78223f60da1f48798a1
dscrane <daegancrane@gmail.com>
2020-04-26 10:04:02 -0400

    yet again

--- scripts/form-data.php
+++ scripts/form-data.php
@@ -1,7 +1,25 @@
-if ( isset( $_POST['submit'] ) ) {
-
-  
-
-  echo "{ <br />\\n" . $_REQUEST['first-name'] . "<br />\\n" . $_REQUEST['last-name'] ."<br />\\n" . $_REQUEST['email'] . "<br />\\n" . $_REQUEST['phone'] . "<br />\\n" . $_REQUEST['message'] . "<br />\\n}";
-  
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact Response</title>
+  </head>
+  <body>
+    <p>
+      {
+      <br />
+      <?php echo $_POST["first-name"]?>
+      <br />
+      <?php echo $_POST["last-name"]?>
+      <br />
+      <?php echo $_POST["email"]?>
+      <br />
+      <?php echo $_POST["phone"]?>
+      <br />
+      <?php echo $_POST["message"]?>
+      <br />
+      }
+    </p>
+  </body>
+</html>
